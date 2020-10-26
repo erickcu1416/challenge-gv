da1dceb891232debb3985d5329677f3e483f7f97
ErickCU1416 <erickchan1416@hotmail.com>
2020-10-26 02:32:56 -0600

    Se agrega componente funcional reminder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CalendarComponent } from './components/calendar/calendar.component';
+import { ReminderComponent } from './components/reminder/reminder.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    ReminderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/reminder/reminder.component.ts
@@ -0,0 +1,34 @@
+import { IReminder } from 'src/interfaces/reminder.interface';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-reminder',
+  templateUrl: './reminder.component.html',
+  styleUrls: ['./reminder.component.scss']
+})
+export class ReminderComponent implements OnInit {
+
+  @Output() addReminderEmitter = new EventEmitter<string>();
+  @Output() deleteAllRemindersByDayEmitter = new EventEmitter<string>();
+  @Output() editReminderEmitter = new EventEmitter<string>();
+
+  @Input() items: IReminder[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  addReminder() {
+    this.addReminderEmitter.emit();
+  }
+
+  deleteAllRemindersByDay() {
+    this.deleteAllRemindersByDayEmitter.emit();
+  }
+
+  editReminder(item) {
+    this.editReminderEmitter.emit(item);
+  }
+
+}
